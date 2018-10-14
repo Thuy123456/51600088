9661da67ff1772a1885ceddf2e47eedea347d26a
Thuy123456 <thuy3982927@gmail.com>
2018-10-14 18:15:59 +0700

    Update HelloWorld.java
    new file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,10 @@ import java.util.*;
 public class HelloWorld
 {   
     public static void main(String[] args){
-		
-        System.out.println("Hello World I am Thuy");   
+		Scanner scanIn = new Scanner(System.in);
+		String name = "";
+		System.out.print("Input your name: ");
+		name = scanIn.nextLine();
+        System.out.println("Hello World I am "+ name +".");   
     }
 } 
