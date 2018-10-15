b6a37447500bad04e58b1f899649a36003bdfbce
Long <nhlong909@gmail.com>
2018-10-15 09:57:09 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,20 @@
 ﻿import java.util.*;
+import java.io.*;
 public class HelloWorld
 {   
     public static void main(String[] args){
-<<<<<<< HEAD
-		
-        System.out.println(“Hello World This is TDTU”);   
-=======
-		Scanner scanIn = new Scanner(System.in);
+        System.out.println("Hello World This is TDTU");
+		//Scanner scanIn = new Scanner(System.in);
+		FileReader f = new FileReader("file.txt");
 		String name = "";
-		System.out.print("Input your name: ");
-		name = scanIn.nextLine();
-        System.out.println("Hello World I am "+ name +".");   
->>>>>>> master
-    }
+		//System.out.print("Input your name: ");
+		int count = 0;
+		Scanner s = new Scanner(f);
+		while(s.hasNextLine()){
+			name = s.nextLine();
+			System.out.println("Hello World I am "+ name +".");
+			count++;
+		}
+		System.out.println("Total " + count + " Students");
+	}
 } 
